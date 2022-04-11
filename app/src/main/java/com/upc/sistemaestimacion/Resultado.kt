b4b1b73291a3bd51acc0400af3ce66afff7f147e
Hillary <hmoscoso@bevertec.com>
2022-04-11 09:13:15 -0500

    Front-End Sistema Estimacion app movil v3.0

--- app/src/main/java/com/upc/sistemaestimacion/Resultado.kt
+++ app/src/main/java/com/upc/sistemaestimacion/Resultado.kt
@@ -3,12 +3,19 @@ package com.upc.sistemaestimacion
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
+import com.github.anastr.speedviewlib.SpeedView
+import com.github.anastr.speedviewlib.Speedometer
 import kotlinx.android.synthetic.main.activity_rango.*
 import kotlinx.android.synthetic.main.activity_resultado.*
 
 class Resultado : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        speedView.speedTo(70F, 4000)
+
     }
 }
